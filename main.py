87b2e131a76626378484951cb20a1f80514af999
codebiscuits <ross_coates82@hotmail.com>
2026-04-18 17:30:50 +0100

    implemented groups

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,13 +9,38 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
         log_state()
+
+        # if user closes the window, kill the process
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # black background fill
         screen.fill((0, 0, 0))
+
+        # update all updatable objects
+        updatable.update(dt)
+
+        # loop through drawable objects and draw them individually
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
+    
+        delta = clock.tick(60)
+        dt = delta / 1000
+        
 
 if __name__ == "__main__":
     main()
